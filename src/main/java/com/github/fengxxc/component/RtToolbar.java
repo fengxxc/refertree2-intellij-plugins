8e1aee21b3e2982fe3720d6739c959242243f85b
fengxxc <fengxxc@gmail.com>
2022-10-06 12:38:21 +0800

    fix: 毙掉启动时不知道干啥用的的警告_(:з)∠)_

--- src/main/java/com/github/fengxxc/component/RtToolbar.java
+++ src/main/java/com/github/fengxxc/component/RtToolbar.java
@@ -1,12 +1,17 @@
 package com.github.fengxxc.component;
 
 import com.intellij.icons.AllIcons;
-import com.intellij.openapi.actionSystem.ActionGroup;
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.ide.DataManager;
+import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.actionSystem.ex.ActionButtonLook;
+import com.intellij.openapi.actionSystem.impl.ActionButton;
 import com.intellij.openapi.actionSystem.impl.ActionToolbarImpl;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.util.IJSwingUtilities;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.awt.*;
 
 /**
  * @author fengxxc
@@ -30,5 +35,28 @@ public class RtToolbar extends ActionToolbarImpl {
             }
         }, true, true);
         // setTargetComponent(this);
+    }
+
+    @Override
+    protected @NotNull ActionButton createToolbarButton(@NotNull AnAction action, ActionButtonLook look, @NotNull String place, @NotNull Presentation presentation, @NotNull Dimension minimumSize) {
+        ActionButton toolbarButton = super.createToolbarButton(action, look, place, presentation, minimumSize);
+        toolbarButton.setFocusable(true);
+        return toolbarButton;
+    }
+
+    @Override
+    protected @NotNull DataContext getDataContext() {
+        final String SUPPRESS_TARGET_COMPONENT_WARNING = "ActionToolbarImpl.suppressTargetComponentWarning";
+        if (super.getTargetComponent() == null && getClientProperty(SUPPRESS_TARGET_COMPONENT_WARNING) == null &&
+                !ApplicationManager.getApplication().isUnitTestMode()) {
+            putClientProperty(SUPPRESS_TARGET_COMPONENT_WARNING, true);
+            /* Override 毙掉警告
+            LOG.warn("'" + myPlace + "' toolbar by default uses any focused component to update its actions. " +
+                    "Toolbar actions that need local UI context would be incorrectly disabled. " +
+                    "Please call toolbar.setTargetComponent() explicitly.", myCreationTrace);
+            */
+        }
+        Component target = super.getTargetComponent() != null ? super.getTargetComponent() : IJSwingUtilities.getFocusedComponentInWindowOrSelf(this);
+        return DataManager.getInstance().getDataContext(target);
     }
 }
